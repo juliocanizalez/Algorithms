dd8d5e1a0c1a5e481433a816271053a57e6fd562
Julio Canizalez <juliocanizalez@outlook.com>
2020-12-21 19:28:02 -0600

    Reverse Integer - easy

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -3,7 +3,7 @@ package leetcode;
 /*Given an array of integers nums and an integer target, 
 return indices of the two numbers such that they add up to target.*/
 
-class Solution{
+class TwoSum{
     public int[] twoSum(int[] nums, int target) {
         for (int i =0; i<nums.length; i++){
             for (int j = i + 1; j<nums.length; j++){
